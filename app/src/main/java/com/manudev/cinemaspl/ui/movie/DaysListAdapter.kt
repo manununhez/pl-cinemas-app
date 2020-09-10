3c8634ffe6250e02cb57014be1e28976a678fbee
Manuel Nunez <03.manu@gmail.com>
2020-09-10 18:44:43 +0200

    Code cleanup

--- app/src/main/java/com/manudev/cinemaspl/ui/movie/DaysListAdapter.kt
+++ app/src/main/java/com/manudev/cinemaspl/ui/movie/DaysListAdapter.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.manudev.cinemaspl.ui.movie
 
 import android.content.Context
@@ -29,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.manudev.cinemaspl.R
 import com.manudev.cinemaspl.databinding.DayTitleItemBinding
 import com.manudev.cinemaspl.ui.SharedMovieViewModel
+import com.manudev.cinemaspl.util.DateUtils
 import com.manudev.cinemaspl.vo.DateTitle
 import java.util.*
 
@@ -102,19 +87,19 @@ class DaysListAdapter(
 
     private fun formatWeekDay(date: String): String {
         val currentDate = Date()
-        val today = DateTitle.dateFormat(currentDate)
-        val todayWeekName = DateTitle.weekDateFormat(currentDate)
+        val today = DateUtils.dateFormat(currentDate)
+        val todayWeekName = DateUtils.weekDateFormat(currentDate)
 
-        val formattedDay = DateTitle.dateFormat(date)
-        val formattedWeekDay = DateTitle.weekDateFormat(date)
+        val formattedDay = DateUtils.dateFormat(date)
+        val formattedWeekDay = DateUtils.weekDateFormat(date)
         return if (today == formattedDay && todayWeekName == formattedWeekDay)
             context.getString(R.string.today)
         else formattedWeekDay
     }
 
-    private fun formatDateDay(date: String) = DateTitle.dayDateFormat(date)
+    private fun formatDateDay(date: String) = DateUtils.dayDateFormat(date)
 
-    private fun formatDateMonth(date: String) = DateTitle.monthDateFormat(date)
+    private fun formatDateMonth(date: String) = DateUtils.monthDateFormat(date)
 }
 
 interface DayTitleViewClickCallback {
