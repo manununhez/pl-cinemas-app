10ded00d85394955038f7dceecc29c907ce1d087
Manuel Nunez <03.manu@gmail.com>
2020-09-30 14:00:23 +0200

    [Bug Fix] API level issue. For android API level below 21 you need to add following dependency in build gradle file - Multidex

--- app/src/main/java/com/manudev/cinemaspl/CinemaPLApplication.kt
+++ app/src/main/java/com/manudev/cinemaspl/CinemaPLApplication.kt
@@ -1,7 +1,7 @@
 package com.manudev.cinemaspl
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CinemaPLApplication: Application()
+class CinemaPLApplication: MultiDexApplication()
